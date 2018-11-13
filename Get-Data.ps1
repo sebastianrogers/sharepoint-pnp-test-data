8a2a77778f9be64d2540dda6271b09306e5bd915
Sebastian Rogers <rogerss@simpleinnovation.co.uk>
2018-11-13 14:24:23 +0000

    Create Get-Data.ps1

---
+++ Get-Data.ps1
@@ -0,0 +1,63 @@
+<#
+.SYNOPSIS
+Gets data from a SharePoint list
+
+.EXAMPLE
+Export the Action Definitions from a demo site.
+
+-Url:http://simpleinnovation.sharepoint.com/sites/demo -List:'Action Definitions'
+
+#>
+[CmdletBinding(SupportsShouldProcess)]
+param(
+    # The URL of the site collection to get the lists data from
+    [Parameter(Mandatory)][string]$URL,
+
+    # The title of the list to get the data from
+    [Parameter(Mandatory)][string]$List,
+
+    # The CSV file to write the data to
+    [Parameter(Mandatory)][string]$Path
+)
+
+$ErrorActionPreference = 'stop'
+$InformationPreference = 'Continue'
+
+if ($VerbosePreference -eq 'Continue') {
+    Set-PnPTraceLog -On -Level:Debug
+}
+else {
+    Set-PnPTraceLog -Off
+}
+
+Connect-PnPOnline -Url:$URL
+
+$Context = Get-PnPContext
+
+$ListItems = Get-PnPListItem `
+    -List:$List
+
+$Append = $false
+
+$ListItems |
+    ForEach-Object {
+        $Item = $PSItem
+
+        $Context.Load($Item.ContentType)
+        $Context.Load($Item.FieldValuesAsText)
+        Execute-PnPQuery
+
+        $Object = New-Object PSObject
+        $Object | Add-Member -MemberType:NoteProperty -Name:"List" -Value:$List
+        $Object | Add-Member -MemberType:NoteProperty -Name:"ContentType" -Value:$Item.ContentType.Name
+
+        $Item.FieldValues.Keys |
+            ForEach-Object {
+                $Key = $PSItem
+                $Object | Add-Member -MemberType:NoteProperty -Name:$Key -Value:$Item.FieldValuesAsText[$Key]
+            }
+
+        Export-Csv -Path:$Path -InputObject:$Object -NoTypeInformation -Append:$Append
+
+        $Append = $true
+    }
